62b5211b5490eb340b6e636926d036ea3be20944
andrew-polk <andrew_polk@sil.org>
2017-03-01 11:32:03 -0500

    Add the first image in the book as the cover image

--- PhotoStoryToBloomConverter/BloomModel/BloomBookData.cs
+++ PhotoStoryToBloomConverter/BloomModel/BloomBookData.cs
@@ -7,99 +7,101 @@ using System.IO;
 
 namespace PhotoStoryToBloomConverter.BloomModel
 {
-    //Comparable to a collection of the bloom 'data' divs
-    public class BloomBookData
-    {
-        public string LanguagesOfBook;
-        public string StyleNumberSequence;
-        public string LicenseUrl = ""; //"http://creativecommons.org/licenses/by/4.0/";
-        public string LicenseImage;
-        public string LicenseNotes;
-        public string Title;
-        public string Copyright;
-        public string[] ContentLanguages;
+	//Comparable to a collection of the bloom 'data' divs
+	public class BloomBookData
+	{
+		public string LanguagesOfBook;
+		public string StyleNumberSequence;
+		public string LicenseUrl = ""; //"http://creativecommons.org/licenses/by/4.0/";
+		public string LicenseImage;
+		public string LicenseNotes;
+		public string Title;
+		public string Copyright;
+		public string[] ContentLanguages;
 
-        public string CoverNarrationPath;
-        public string CoverBackgroundAudioPath;
-        public double CoverBackgroundAudioVolume;
+		public string CoverImage { get; set; }
+		public string CoverNarrationPath;
+		public string CoverBackgroundAudioPath;
+		public double CoverBackgroundAudioVolume;
 
-        //All localized variables are expected to have similar indexes
-        //to their languages location in ContentLanguages
-        public string[] LocalizedBookTitle;
-        public string[] LocalizedSmallCoverCredits;
-        public string[] LocalizedOriginalContributions;
-        public string[] LocalizedFunding;
-        public List<string> LocalizedOriginalAcknowledgments;
-        public string[] LocalizedInsideFrontCover;
-        public string[] LocalizedInsideBackCover;
-        public string[] LocalizedOutsideBackCover;
-        public string[] LocalizedLicenseDescription;
+		//All localized variables are expected to have similar indexes
+		//to their languages location in ContentLanguages
+		public string[] LocalizedBookTitle;
+		public string[] LocalizedSmallCoverCredits;
+		public string[] LocalizedOriginalContributions;
+		public string[] LocalizedFunding;
+		public List<string> LocalizedOriginalAcknowledgments;
+		public string[] LocalizedInsideFrontCover;
+		public string[] LocalizedInsideBackCover;
+		public string[] LocalizedOutsideBackCover;
+		public string[] LocalizedLicenseDescription;
 
-        public List<List<Dictionary<string, string>>> LocalizedNarrationList;
+		public List<List<Dictionary<string, string>>> LocalizedNarrationList;
 
-        public static BloomBookData DefaultBloomBookData(string title)
-        {
-            return new BloomBookData
-            {
-                Title = title,
-                LanguagesOfBook = "English",
-                StyleNumberSequence = "0",
-                ContentLanguages = new [] { "en" },
-                Copyright = "© [year] Sweet Publishing",
-                LicenseNotes = "The license on this book is to be determined",
-                LocalizedBookTitle = new [] { "" },
-                LocalizedSmallCoverCredits = new[] { "" },
-                LocalizedOriginalContributions = new[] { "" },
-                LocalizedFunding = new[] { "" },
-                LocalizedOriginalAcknowledgments = new List<string>(),
-                LocalizedInsideFrontCover = new[] { "" },
-                LocalizedInsideBackCover = new[] { "" },
-                LocalizedOutsideBackCover = new[] { "" },
-                LocalizedLicenseDescription = new[] { "" },
-            };
-        }
+		public static BloomBookData DefaultBloomBookData(string title)
+		{
+			return new BloomBookData
+			{
+				Title = title,
+				LanguagesOfBook = "English",
+				StyleNumberSequence = "0",
+				ContentLanguages = new [] { "en" },
+				Copyright = "© [year] Sweet Publishing",
+				LicenseNotes = "The license on this book is to be determined",
+				LocalizedBookTitle = new [] { "" },
+				LocalizedSmallCoverCredits = new[] { "" },
+				LocalizedOriginalContributions = new[] { "" },
+				LocalizedFunding = new[] { "" },
+				LocalizedOriginalAcknowledgments = new List<string>(),
+				LocalizedInsideFrontCover = new[] { "" },
+				LocalizedInsideBackCover = new[] { "" },
+				LocalizedOutsideBackCover = new[] { "" },
+				LocalizedLicenseDescription = new[] { "" },
+			};
+		}
 
-        public Div ConvertToHtml()
-        {
-            var dataDiv = new Div
-            {
-                Id = "bloomDataDiv",
-                Divs = new List<Div>
-                {
-                    new Div { DataBook = "styleNumberSequence", Lang = "*", SimpleText = StyleNumberSequence},
-                    new Div { DataBook = "languagesOfBook", Lang = "*", SimpleText = LanguagesOfBook},
-                    new Div { DataBook = "copyright", Lang = "*", SimpleText = Copyright},
+		public Div ConvertToHtml()
+		{
+			var dataDiv = new Div
+			{
+				Id = "bloomDataDiv",
+				Divs = new List<Div>
+				{
+					new Div { DataBook = "coverImage", Lang = "*", SimpleText = CoverImage},
+					new Div { DataBook = "styleNumberSequence", Lang = "*", SimpleText = StyleNumberSequence},
+					new Div { DataBook = "languagesOfBook", Lang = "*", SimpleText = LanguagesOfBook},
+					new Div { DataBook = "copyright", Lang = "*", SimpleText = Copyright},
 					new Div { DataBook = "licenseNotes", Lang = "*", SimpleText = LicenseNotes },
 					//new Div { DataBook = "licenseUrl", Lang = "*", SimpleText = LicenseUrl },
 				}
-            };
+			};
 			if (CoverBackgroundAudioPath != null)
-	            dataDiv.Divs.Add(new Div { DataBookAttributes = "frontCover", BackgroundAudio = GetBackgroundAudio(), BackgroundAudioVolume = CoverBackgroundAudioVolume.ToString(CultureInfo.InvariantCulture) });
-            dataDiv.Divs.AddRange(ContentLanguages.Select((lang, index) => new Div { DataBook = string.Format("contentLanguage{0}", index+1), Lang = "*", SimpleText = lang }).ToArray());
-            dataDiv.Divs.AddRange(LocalizedBookTitle.Select((lang, index) => new Div { DataBook = "bookTitle", Lang = ContentLanguages[index], FormattedText = new List<Paragraph> { new Paragraph { Span = new Span { Id = Path.GetFileNameWithoutExtension(CoverNarrationPath), Class = "audio-sentence", ContentText = Title } } } }).ToArray());
-            dataDiv.Divs.AddRange(LocalizedSmallCoverCredits.Select((credits, index) => new Div { DataBook = "smallCoverCredits", Lang = ContentLanguages[index], FormattedText = new List<Paragraph> { new Paragraph { Text = credits } } }).ToArray());
-            dataDiv.Divs.AddRange(LocalizedOriginalContributions.Select((contributions, index) => new Div { DataBook = "originalContributions", Lang = ContentLanguages[index], FormattedText = new List<Paragraph> { new Paragraph { Text = contributions } } }).ToArray());
-            dataDiv.Divs.AddRange(LocalizedOriginalAcknowledgments.Select((acknowledgments, index) => new Div { DataBook = "originalAcknowledgments", Lang = ContentLanguages[index], FormattedText = GetMultiParagraphFromString(acknowledgments) }).ToArray());
-            dataDiv.Divs.AddRange(LocalizedFunding.Select((funding, index) => new Div { DataBook = "funding", Lang = ContentLanguages[index], FormattedText = new List<Paragraph> { new Paragraph { Text = funding } } }).ToArray());
-            dataDiv.Divs.AddRange(LocalizedInsideFrontCover.Select((insideFrontCover, index) => new Div { DataBook = "insideFrontCover", Lang = ContentLanguages[index], FormattedText = new List<Paragraph> { new Paragraph { Text = insideFrontCover } } }).ToArray());
-            dataDiv.Divs.AddRange(LocalizedInsideBackCover.Select((insideBackCover, index) => new Div { DataBook = "insideBackCover", Lang = ContentLanguages[index], FormattedText = new List<Paragraph> { new Paragraph { Text = insideBackCover } } }).ToArray());
-            dataDiv.Divs.AddRange(LocalizedOutsideBackCover.Select((outsideBackCover, index) => new Div { DataBook = "outsideBackCover", Lang = ContentLanguages[index], FormattedText = new List<Paragraph> { new Paragraph { Text = outsideBackCover } } }).ToArray());
-            return dataDiv;
-        }
+				dataDiv.Divs.Add(new Div { DataBookAttributes = "frontCover", BackgroundAudio = GetBackgroundAudio(), BackgroundAudioVolume = CoverBackgroundAudioVolume.ToString(CultureInfo.InvariantCulture) });
+			dataDiv.Divs.AddRange(ContentLanguages.Select((lang, index) => new Div { DataBook = string.Format("contentLanguage{0}", index+1), Lang = "*", SimpleText = lang }).ToArray());
+			dataDiv.Divs.AddRange(LocalizedBookTitle.Select((lang, index) => new Div { DataBook = "bookTitle", Lang = ContentLanguages[index], FormattedText = new List<Paragraph> { new Paragraph { Span = new Span { Id = Path.GetFileNameWithoutExtension(CoverNarrationPath), Class = "audio-sentence", ContentText = Title } } } }).ToArray());
+			dataDiv.Divs.AddRange(LocalizedSmallCoverCredits.Select((credits, index) => new Div { DataBook = "smallCoverCredits", Lang = ContentLanguages[index], FormattedText = new List<Paragraph> { new Paragraph { Text = credits } } }).ToArray());
+			dataDiv.Divs.AddRange(LocalizedOriginalContributions.Select((contributions, index) => new Div { DataBook = "originalContributions", Lang = ContentLanguages[index], FormattedText = new List<Paragraph> { new Paragraph { Text = contributions } } }).ToArray());
+			dataDiv.Divs.AddRange(LocalizedOriginalAcknowledgments.Select((acknowledgments, index) => new Div { DataBook = "originalAcknowledgments", Lang = ContentLanguages[index], FormattedText = GetMultiParagraphFromString(acknowledgments) }).ToArray());
+			dataDiv.Divs.AddRange(LocalizedFunding.Select((funding, index) => new Div { DataBook = "funding", Lang = ContentLanguages[index], FormattedText = new List<Paragraph> { new Paragraph { Text = funding } } }).ToArray());
+			dataDiv.Divs.AddRange(LocalizedInsideFrontCover.Select((insideFrontCover, index) => new Div { DataBook = "insideFrontCover", Lang = ContentLanguages[index], FormattedText = new List<Paragraph> { new Paragraph { Text = insideFrontCover } } }).ToArray());
+			dataDiv.Divs.AddRange(LocalizedInsideBackCover.Select((insideBackCover, index) => new Div { DataBook = "insideBackCover", Lang = ContentLanguages[index], FormattedText = new List<Paragraph> { new Paragraph { Text = insideBackCover } } }).ToArray());
+			dataDiv.Divs.AddRange(LocalizedOutsideBackCover.Select((outsideBackCover, index) => new Div { DataBook = "outsideBackCover", Lang = ContentLanguages[index], FormattedText = new List<Paragraph> { new Paragraph { Text = outsideBackCover } } }).ToArray());
+			return dataDiv;
+		}
 
-	    private static List<Paragraph> GetMultiParagraphFromString(string input)
-	    {
-		    var result = new List<Paragraph>();
-		    foreach (var paragraph in input.Split(new[] { "\r\n" }, StringSplitOptions.None))
-			    result.Add(new Paragraph {Text = paragraph});
-		    return result;
-	    }
+		private static List<Paragraph> GetMultiParagraphFromString(string input)
+		{
+			var result = new List<Paragraph>();
+			foreach (var paragraph in input.Split(new[] { "\r\n" }, StringSplitOptions.None))
+				result.Add(new Paragraph {Text = paragraph});
+			return result;
+		}
 
-	    public string GetBackgroundAudio()
-        {
-            if (!string.IsNullOrWhiteSpace(CoverBackgroundAudioPath))
-                return CoverBackgroundAudioPath;
-            return null;
-        }
-    }
+		public string GetBackgroundAudio()
+		{
+			if (!string.IsNullOrWhiteSpace(CoverBackgroundAudioPath))
+				return CoverBackgroundAudioPath;
+			return null;
+		}
+	}
 }
--- PhotoStoryToBloomConverter/BloomModel/BloomDocument.cs
+++ PhotoStoryToBloomConverter/BloomModel/BloomDocument.cs
@@ -12,137 +12,145 @@ using SIL.Windows.Forms.ImageToolbox;
 
 namespace PhotoStoryToBloomConverter.BloomModel
 {
-    //Comparable to the 'Html' object
-    public class BloomDocument
-    {
-        private readonly BloomMetadata _metadata;
-        private readonly BloomBookData _bookData;
-        private readonly List<BloomPage> _pages = new List<BloomPage>();
-	    private readonly CreditsAndCoverExtractor.ImageIP _imageCopyrightAndLicense;
-	    private readonly Dictionary<string, string> _duplicateAudioFiles;
+	//Comparable to the 'Html' object
+	public class BloomDocument
+	{
+		private readonly BloomMetadata _metadata;
+		private readonly BloomBookData _bookData;
+		private readonly List<BloomPage> _pages = new List<BloomPage>();
+		private readonly CreditsAndCoverExtractor.ImageIP _imageCopyrightAndLicense;
+		private readonly Dictionary<string, string> _duplicateAudioFiles;
 
 		public BloomDocument(PhotoStoryProject project, string bookName, string bookDirectoryPath, IList<List<KeyValuePair<Language, string>>> narratedText,
 			Dictionary<string, string> duplicateAudioFiles)
-        {
-            _metadata = BloomMetadata.DefaultBloomMetadata(bookName);
-            _bookData = BloomBookData.DefaultBloomBookData(bookName);
-	        _duplicateAudioFiles = duplicateAudioFiles;
+		{
+			_metadata = BloomMetadata.DefaultBloomMetadata(bookName);
+			_bookData = BloomBookData.DefaultBloomBookData(bookName);
+			_duplicateAudioFiles = duplicateAudioFiles;
 
-            //A little bit of book-keeping, we want to remove images that are cover or credit images from the final directory
-            var imagePathsToRemove = new SortedSet<string>();
+			//A little bit of book-keeping, we want to remove images that are cover or credit images from the final directory
+			var imagePathsToRemove = new SortedSet<string>();
 
-	        bool creditsFound = false;
+			bool creditsFound = false;
+			bool coverImageFound = false;
 
-            //For each visual unit create a bloom page
-            //Instead of creating a page for cover and credits pages, put information into the book data (data-div)
-            //The X-Matter pack will create more visually appealing cover pages
-            for (var i = 0; i < project.VisualUnits.Length; i++)
-            {
+			//For each visual unit create a bloom page
+			//Instead of creating a page for cover and credits pages, put information into the book data (data-div)
+			//The X-Matter pack will create more visually appealing cover pages
+			for (var i = 0; i < project.VisualUnits.Length; i++)
+			{
 				// With the latest templates (Jan 2017), we have been told that the next to last slide is always unnarrated
 				// and only placed as a pause for reflection. We can ignore it.
-	            if (i == project.VisualUnits.Length - 2)
-		            continue;
+				if (i == project.VisualUnits.Length - 2)
+					continue;
 
-                var visualUnit = project.VisualUnits[i];
-                var psImage = visualUnit.Image;
-                var backgroundAudioPath = GetBackgroundAudioPathForImage(psImage);
-	            var backgroundAudioVolume = backgroundAudioPath == null ? 0.0 : GetBackgroundAudioVolumeForImage(psImage);
+				var visualUnit = project.VisualUnits[i];
+				var psImage = visualUnit.Image;
+				var backgroundAudioPath = GetBackgroundAudioPathForImage(psImage);
+				var backgroundAudioVolume = backgroundAudioPath == null ? 0.0 : GetBackgroundAudioVolumeForImage(psImage);
 
-                var extractor = new CreditsAndCoverExtractor();
+				var extractor = new CreditsAndCoverExtractor();
 				extractor.Extract(bookName, Path.Combine(bookDirectoryPath, psImage.Path));
-                if (extractor.IsCreditsOrCoverPage)
-                {
-                    //If it was a credits page, put credit information into the data divs
-                    if (extractor.CreditString != null)
-                    {
-	                    creditsFound = true;
-                        _bookData.LocalizedOriginalAcknowledgments.Add(extractor.CreditString);
-	                    _imageCopyrightAndLicense = extractor.ImageCopyrightAndLicense;
-                    }
+				if (extractor.IsCreditsOrCoverPage)
+				{
+					//If it was a credits page, put credit information into the data divs
+					if (extractor.CreditString != null)
+					{
+						creditsFound = true;
+						_bookData.LocalizedOriginalAcknowledgments.Add(extractor.CreditString);
+						_imageCopyrightAndLicense = extractor.ImageCopyrightAndLicense;
+					}
 
-                    //If the image had narration and/or background audio, and was the front cover, we want to store the audio for the new cover page
-                    if (i == 0 && backgroundAudioPath != null)
-                    {
-                        _bookData.CoverBackgroundAudioPath = backgroundAudioPath;
-                        _bookData.CoverBackgroundAudioVolume = backgroundAudioVolume;
+					//If the image had narration and/or background audio, and was the front cover, we want to store the audio for the new cover page
+					if (i == 0 && backgroundAudioPath != null)
+					{
+						_bookData.CoverBackgroundAudioPath = backgroundAudioPath;
+						_bookData.CoverBackgroundAudioVolume = backgroundAudioVolume;
 
-                    }
-                    if (i == 0 && visualUnit.Narration != null)
-                        _bookData.CoverNarrationPath = visualUnit.Narration.Path;
+					}
+					if (i == 0 && visualUnit.Narration != null)
+						_bookData.CoverNarrationPath = visualUnit.Narration.Path;
 
-                    imagePathsToRemove.Add(Path.Combine(bookDirectoryPath, psImage.Path));
-                }
-                else
-                {
-                    var cropRectangle = new Rectangle();
-                    //If the image photostory was using had a crop edit, we need to adjust the image displayed for bloom likewise
-                    if (psImage.Edits != null)
-                    {
-                        foreach (var edit in psImage.Edits)
-                        {
-                            if (edit.RotateAndCrop == null) continue;
-                            cropRectangle = edit.RotateAndCrop.CroppedRect.ToAnimationRectangle();
-                        }
-                    }
+					imagePathsToRemove.Add(Path.Combine(bookDirectoryPath, psImage.Path));
+				}
+				else
+				{
+					var cropRectangle = new Rectangle();
+					//If the image photostory was using had a crop edit, we need to adjust the image displayed for bloom likewise
+					if (psImage.Edits != null)
+					{
+						foreach (var edit in psImage.Edits)
+						{
+							if (edit.RotateAndCrop == null) continue;
+							cropRectangle = edit.RotateAndCrop.CroppedRect.ToAnimationRectangle();
+						}
+					}
 
-                    var bloomImage = new BloomImage
-                    {
-                        Src = psImage.Path,
-                        ImageSize = new Size { Height = psImage.AbsoluteMotion.BaseImageHeight, Width = psImage.AbsoluteMotion.BaseImageWidth },
-                        ImageMotion = new BloomImageMotion
-                        {
-                            CropRectangle = cropRectangle,
-                            InitialImageRectangle = psImage.AbsoluteMotion.Rects[0].ToAnimationRectangle(),
-                            FinalImageRectangle = psImage.AbsoluteMotion.Rects[1].ToAnimationRectangle(),
-                        }
-                    };
+					var bloomImage = new BloomImage
+					{
+						Src = psImage.Path,
+						ImageSize = new Size { Height = psImage.AbsoluteMotion.BaseImageHeight, Width = psImage.AbsoluteMotion.BaseImageWidth },
+						ImageMotion = new BloomImageMotion
+						{
+							CropRectangle = cropRectangle,
+							InitialImageRectangle = psImage.AbsoluteMotion.Rects[0].ToAnimationRectangle(),
+							FinalImageRectangle = psImage.AbsoluteMotion.Rects[1].ToAnimationRectangle(),
+						}
+					};
 
-                    List<KeyValuePair<Language, string>> text = null;
-                    if (narratedText != null && narratedText.Count > i)
-                        text = narratedText[i];
+					List<KeyValuePair<Language, string>> text = null;
+					if (narratedText != null && narratedText.Count > i)
+						text = narratedText[i];
 
-                    var narrationPath = "";
-                    if (visualUnit.Narration != null)
-                        narrationPath = visualUnit.Narration.Path;
+					var narrationPath = "";
+					if (visualUnit.Narration != null)
+						narrationPath = visualUnit.Narration.Path;
 
-	                var narrationFilePath = Path.Combine(bookDirectoryPath, BloomAudio.kAudioDirectory, narrationPath);
-                    var bloomAudio = new BloomAudio(narrationPath, backgroundAudioPath, backgroundAudioVolume, GetDuration(narrationFilePath));
+					var narrationFilePath = Path.Combine(bookDirectoryPath, BloomAudio.kAudioDirectory, narrationPath);
+					var bloomAudio = new BloomAudio(narrationPath, backgroundAudioPath, backgroundAudioVolume, GetDuration(narrationFilePath));
 
-                    _pages.Add(new BloomPage(bloomImage, text, bloomAudio));
-                }
-            }
+					if (!coverImageFound)
+					{
+						_bookData.CoverImage = bloomImage.Src;
+						coverImageFound = true;
+					}
+
+					_pages.Add(new BloomPage(bloomImage, text, bloomAudio));
+				}
+			}
 			if (!creditsFound)
 				Console.WriteLine("Credits not processed for {0}", bookName);
 			else
 				CreditsAndCoverExtractor.CreateMapFile();
 
-	        if (_imageCopyrightAndLicense != CreditsAndCoverExtractor.ImageIP.Unknown)
-	        {
-		        //Because credits may have been at end of book, go back through and set image credits if we extracted some.
-		        foreach (var page in _pages)
-		        {
-			        var imageLocation = Path.Combine(bookDirectoryPath, page.ImageAndTextWithAudioSplitter.Image.Src);
-			        using (var image = PalasoImage.FromFile(imageLocation))
-			        {
-				        if (_imageCopyrightAndLicense == CreditsAndCoverExtractor.ImageIP.SweetPublishingAndWycliffe && IsWycliffeImage(bookName, image.FileName))
-					        ApplyWycliffeIPInfoForImages(image);
-				        else
-					        ApplySweetPublishingIPInfoForImages(image);
+			if (_imageCopyrightAndLicense != CreditsAndCoverExtractor.ImageIP.Unknown)
+			{
+				//Because credits may have been at end of book, go back through and set image credits if we extracted some.
+				foreach (var page in _pages)
+				{
+					var imageLocation = Path.Combine(bookDirectoryPath, page.ImageAndTextWithAudioSplitter.Image.Src);
+					using (var image = PalasoImage.FromFile(imageLocation))
+					{
+						if (_imageCopyrightAndLicense == CreditsAndCoverExtractor.ImageIP.SweetPublishingAndWycliffe && IsWycliffeImage(bookName, image.FileName))
+							ApplyWycliffeIPInfoForImages(image);
+						else
+							ApplySweetPublishingIPInfoForImages(image);
 
-				        image.SaveUpdatedMetadataIfItMakesSense();
-			        }
-		        }
-	        }
-	        else
-	        {
-		        Debug.Fail("Image copyright and license information unknown for {0}", bookName);
-	        }
-            foreach (var imagePath in imagePathsToRemove)
-            {
-                File.Delete(imagePath);
-            }
+						image.SaveUpdatedMetadataIfItMakesSense();
+					}
+				}
+			}
+			else
+			{
+				Debug.Fail("Image copyright and license information unknown for {0}", bookName);
+			}
+			foreach (var imagePath in imagePathsToRemove)
+			{
+				File.Delete(imagePath);
+			}
 		}
 
+		// ENHANCE: use image hashes to ensure we are applying to the correct ones if order changes
 		private bool IsWycliffeImage(string bookName, string imagePath)
 		{
 			// Unfortunate we can't get this information from the project in some consistent way.
@@ -181,42 +189,42 @@ namespace PhotoStoryToBloomConverter.BloomModel
 		}
 
 		private double GetBackgroundAudioVolumeForImage(Ps3Image image)
-        {
-            //Converting from 1-100 to 0.01-1.00
-            return image.MusicTracks.First().Volume / 100.00;
-        }
+		{
+			//Converting from 1-100 to 0.01-1.00
+			return image.MusicTracks.First().Volume / 100.00;
+		}
 
-        private string GetBackgroundAudioPathForImage(Ps3Image image)
-        {
-	        if (image.MusicTracks != null && image.MusicTracks.First().SoundTracks != null)
-	        {
-		        var audioPath = image.MusicTracks.First().SoundTracks.First().Path;
-		        string realPath;
+		private string GetBackgroundAudioPathForImage(Ps3Image image)
+		{
+			if (image.MusicTracks != null && image.MusicTracks.First().SoundTracks != null)
+			{
+				var audioPath = image.MusicTracks.First().SoundTracks.First().Path;
+				string realPath;
 				if (_duplicateAudioFiles.TryGetValue(audioPath, out realPath))
 					return realPath;
-		        return audioPath;
-	        }
-	        return null;
-        }
+				return audioPath;
+			}
+			return null;
+		}
 
-        public Html ConvertToHtml()
-        {
-            var html = new Html
-            {
-                Head = _metadata.ConvertToHtml(),
-                Body = new Body
-                {
-                    BookCreationType = "original",
-                    Class = "publishMode",
-                    Divs = new List<Div>
-                    {
-                        _bookData.ConvertToHtml(),
-                    }
-                }
-            };
-            foreach (var page in _pages)
-                html.Body.Divs.Add(page.ConvertToHtml());
-            return html;
-        }
-    }
+		public Html ConvertToHtml()
+		{
+			var html = new Html
+			{
+				Head = _metadata.ConvertToHtml(),
+				Body = new Body
+				{
+					BookCreationType = "original",
+					Class = "publishMode",
+					Divs = new List<Div>
+					{
+						_bookData.ConvertToHtml(),
+					}
+				}
+			};
+			foreach (var page in _pages)
+				html.Body.Divs.Add(page.ConvertToHtml());
+			return html;
+		}
+	}
 }
